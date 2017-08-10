2b714d4e837fd68b1ff2e1789d1a76f7230b570d
Josu <josufmc@hotmail.com>
2017-08-10 12:39:42 +0200

    Products service finished

--- application/controllers/Productos.php
+++ application/controllers/Productos.php
@@ -25,4 +25,32 @@ class Productos extends REST_Controller {
 		);
 		return $this->response($response);
 	}
+
+	public function por_tipo_get($tipo=0, $pagina=0){
+		if ($tipo==0){
+			$response = array(
+				'error' => TRUE,
+				'mensaje' => 'Falta el parámetro de tipo'
+			);
+			return $this->response($response, REST_Controller::HTTP_BAD_REQUEST);
+		}
+		
+		$pagina = $pagina * 10;
+		$query = $this->db->query("SELECT * FROM productos WHERE linea_id = " . $this->db->escape($tipo) . "  LIMIT " . $this->db->escape($pagina) . ", 10");
+		$response = array(
+			'error' => FALSE,
+			'productos' => $query->result()
+		);
+		return $this->response($response);
+	}
+
+	public function buscar_get($termino = 'no especif'){
+		$query = $this->db->query("SELECT * FROM productos WHERE producto LIKE " . $this->db->escape("%" . $termino . "%"));
+		$response = array(
+			'error' => FALSE,
+			'termino' => $termino,
+			'productos' => $query->result()
+		);
+		return $this->response($response);
+	}
 }
